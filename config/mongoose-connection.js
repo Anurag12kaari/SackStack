aed06cfca6cd100f0dd6ae2afe6ee76f9162c0b3
Anurag Dutta <anuragdutta2004@gmail.com>
2024-12-25 01:20:04 +0530

    upload

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+const dbgr = require("debug")("development:mongoose");
 mongoose
     .connect("mongodb://127.0.0.1:27017/bags")
     .then(function () {
-        console.log("connected")
+        console.log("connection Build");
     })
     .catch(function (err) {
         console.log(err);
