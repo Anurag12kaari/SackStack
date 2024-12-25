e4b195282aaa1c7cb2135705f4a167bf87ceecdc
Anurag Dutta <anuragdutta2004@gmail.com>
2024-12-26 00:59:16 +0530

    upload

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const path = require("path");
 const db = require("./config/mongoose-connection");
+const index = require("./routes/index")
 const ownersRouter = require("./routes/ownersRouter");
 const productRouter = require("./routes/productRouter");
 const userRouter = require("./routes/userRouter");
@@ -11,6 +12,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
+app.use("/",index);
 app.use("/owners",ownersRouter);
 app.use("/users",userRouter);
 app.use("/products",productRouter);
